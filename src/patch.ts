b431313022dbb7efc7fe269a1c0623d1a6a923b9
Aiden Bai <aiden.bai05@gmail.com>
2021-07-08 16:06:29 -0700

    feat: proper diffing for all deltas

--- src/patch.ts
+++ src/patch.ts
@@ -2,7 +2,6 @@
 
 import { OLD_VNODE_FIELD } from './constants';
 import { createElement } from './createElement';
-import { diff } from './diff';
 import { VDeltas, VElement, VFlags, VNode, VProps } from './structs';
 
 /**
@@ -50,13 +49,20 @@ export const patchChildren = (
       (delta: VDeltas, oldVElement?: VElement, newVElement?: VElement, position?: number): void => {
         switch (delta) {
           case VDeltas.CREATE:
-            el.insertBefore(createElement(newVElement!), el.childNodes[position!]);
+            el.insertBefore(createElement(newVElement!), el.childNodes[position!] || null);
             break;
           case VDeltas.UPDATE:
-            patch(<HTMLElement>el.childNodes[position!], newVElement!, oldVElement);
+            patch(
+              <HTMLElement>el.childNodes[oldVNodeChildren.indexOf(oldVElement!)],
+              newVElement!,
+              oldVElement,
+            );
             break;
           case VDeltas.MOVE:
-            el.insertBefore(patch(el, newVElement!, oldVElement), el.childNodes[position!]);
+            console.log(position);
+            // eslint-disable-next-line no-case-declarations
+            const child = <HTMLElement>el.childNodes[oldVNodeChildren.indexOf(oldVElement!)];
+            el.insertBefore(patch(child, newVElement!, oldVElement), child || null);
             break;
           case VDeltas.REMOVE:
             el.removeChild(<HTMLElement>el.childNodes[position!]);
@@ -152,4 +158,153 @@ export const patch = (
   if (!prevVNode) el[OLD_VNODE_FIELD] = newVNode;
 
   return el;
+};
+
+// TODO: add attribution comment to JSDoc
+export const diff = (
+  oldVElementChildren: VElement[],
+  newVElementChildren: VElement[],
+  effect: (
+    delta: VDeltas,
+    oldVElement?: VElement,
+    newVElement?: VElement,
+    position?: number,
+  ) => void,
+): void => {
+  let oldStartIdx = 0;
+  let newStartIdx = 0;
+  let oldEndIdx = oldVElementChildren.length - 1;
+  let newEndIdx = newVElementChildren.length - 1;
+  let oldStartVElement = oldVElementChildren[oldStartIdx];
+  let newStartVElement = newVElementChildren[newStartIdx];
+
+  // List head is the same
+  while (
+    oldStartIdx <= oldEndIdx &&
+    newStartIdx <= newEndIdx &&
+    oldStartVElement.key === newStartVElement.key
+  ) {
+    effect(VDeltas.UPDATE, oldStartVElement, newStartVElement, newStartIdx);
+    oldStartVElement = oldVElementChildren[++oldStartIdx];
+    newStartVElement = newVElementChildren[++newStartIdx];
+  }
+
+  // The above case is orders of magnitude more common than the others, so fast-path it
+  if (newStartIdx > newEndIdx && oldStartIdx > oldEndIdx) return;
+
+  let oldEndVElement = oldVElementChildren[oldEndIdx];
+  let newEndVElement = newVElementChildren[newEndIdx];
+  let movedFromFront = 0;
+
+  // Reversed
+  while (
+    oldStartIdx <= oldEndIdx &&
+    newStartIdx <= newEndIdx &&
+    oldStartVElement.key === newEndVElement.key
+  ) {
+    effect(VDeltas.MOVE, oldStartVElement, newEndVElement, oldEndIdx - movedFromFront + 1);
+    oldStartVElement = oldVElementChildren[++oldStartIdx];
+    newEndVElement = newVElementChildren[--newEndIdx];
+    ++movedFromFront;
+  }
+
+  // Reversed the other way (in case of e.g. reverse and append)
+  while (
+    oldEndIdx >= oldStartIdx &&
+    newStartIdx <= newEndIdx &&
+    newStartVElement.key === oldEndVElement.key
+  ) {
+    effect(VDeltas.MOVE, oldEndVElement, newStartVElement, newStartIdx);
+    oldEndVElement = oldVElementChildren[--oldEndIdx];
+    newStartVElement = newVElementChildren[++newStartIdx];
+    --movedFromFront;
+  }
+
+  // List tail is the same
+  while (
+    oldEndIdx >= oldStartIdx &&
+    newEndIdx >= newStartIdx &&
+    oldEndVElement.key &&
+    newEndVElement.key
+  ) {
+    effect(VDeltas.UPDATE, oldEndVElement, newEndVElement, newEndIdx);
+    oldEndVElement = oldVElementChildren[--oldEndIdx];
+    newEndVElement = newVElementChildren[--newEndIdx];
+  }
+
+  if (oldStartIdx > oldEndIdx) {
+    while (newStartIdx <= newEndIdx) {
+      effect(VDeltas.CREATE, undefined, newStartVElement, newStartIdx);
+      newStartVElement = newVElementChildren[++newStartIdx];
+    }
+
+    return;
+  }
+
+  if (newStartIdx > newEndIdx) {
+    while (oldStartIdx <= oldEndIdx) {
+      effect(VDeltas.REMOVE, oldStartVElement);
+      oldStartVElement = oldVElementChildren[++oldStartIdx];
+    }
+
+    return;
+  }
+
+  let created = 0;
+  let pivotDest = null;
+  let pivotIdx = oldStartIdx - movedFromFront;
+  const memoBase = oldStartIdx;
+  const memo = new Uint32Array(Math.ceil(oldEndIdx - oldStartIdx / 32));
+  const oldMap = {};
+
+  for (let i = oldStartIdx; i < oldEndIdx + 1; ++i) {
+    oldMap[oldVElementChildren[i].key!] = i;
+  }
+
+  for (; newStartIdx <= newEndIdx; newStartVElement = newVElementChildren[++newStartIdx]) {
+    const oldIdx = oldMap[newStartVElement.key!];
+
+    if (oldIdx === undefined) {
+      effect(VDeltas.CREATE, undefined, newStartVElement, pivotIdx++);
+      ++created;
+    } else if (oldStartIdx !== oldIdx) {
+      effect(VDeltas.MOVE, oldVElementChildren[oldIdx], newStartVElement, pivotIdx++);
+      const idx = oldIdx - memoBase;
+      const result = idx % 32;
+      const position = (idx - result) / 32;
+      memo[position] |= 1 << result;
+    } else {
+      pivotDest = newStartIdx;
+    }
+  }
+
+  if (pivotDest !== null) {
+    memo[0] |= 1 << 0;
+    effect(
+      VDeltas.MOVE,
+      oldVElementChildren[oldStartIdx],
+      newVElementChildren[pivotDest],
+      pivotDest,
+    );
+  }
+
+  // If there are no creations, then you have to
+  // remove exactly max(prevLen - nextLen, 0) elements in this
+  // diff. You have to remove one more for each element
+  // that was created. This means once we have
+  // removed that many, we can stop.
+  const necessaryRemovals = oldVElementChildren.length - newVElementChildren.length + created;
+  for (
+    let removals = 0;
+    removals < necessaryRemovals;
+    oldStartVElement = oldVElementChildren[++oldStartIdx]
+  ) {
+    const idx = oldStartIdx - memoBase;
+    const result = idx % 32;
+    const position = (idx - result) / 32;
+    if (!(memo[position] & (1 << result))) {
+      effect(VDeltas.REMOVE, oldStartVElement);
+      ++removals;
+    }
+  }
 };
